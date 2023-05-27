a0aeeb2ef98c6b92398923e1deca7216dc390158
David Kerkeslager <david@kerkeslager.com>
2023-05-27 16:41:34 -0400

    Extend test harness to be able to filter tests

--- unit_test.c.sh
+++ unit_test.c.sh
@@ -1,11 +1,13 @@
 echo '#include <stdio.h>'
 echo '#include <stdlib.h>'
+echo '#include <string.h>'
 echo
 find . -name '*.h' | sed 's/.*/#include "&"/g'
 echo
-echo 'int main() {'
+echo '#define DO_TEST(t) if(argc == 1 || strstr(#t, argv[1])) { t(); count++; printf("."); }'
+echo 'int main(int argc, char* argv[]) {'
 echo '  size_t count = 0;'
-grep --no-filename 'void test_' *.h | sed -E 's/void (test_[A-Za-z_]*)\(\);/  \1(); count++; printf(".");/g'
+grep --no-filename 'void test_' *.h | sed -E 's/void (test_[A-Za-z_]*)\(\);/  DO_TEST(\1);/g'
 echo '  printf("\\n%zu TESTS PASSED\\n\\n", count);'
 echo '  return 0;'
 echo '}'
