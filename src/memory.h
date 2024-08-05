3ef1bd1ed18707cdc315f76ce77a474415fc4c4c
David Kerkeslager <david@kerkeslager.com>
2024-08-05 09:37:46 -0400

    Wrap memory functions for testing

--- src/memory.h
+++ src/memory.h
@@ -4,18 +4,49 @@
 #include <assert.h>
 #include <stdlib.h>
 
-inline void* Memory_allocate(size_t size) {
-  void* result = malloc(size);
+#ifdef TEST
+  #define TEST_MAX_ALLOCATIONS 16
+  static size_t _allocationCount;
+  static size_t _freeCount;
+  static size_t _allocationList[TEST_MAX_ALLOCATIONS];
+  static void* _freeList[TEST_MAX_ALLOCATIONS];
 
-  // TODO Handle this
-  assert(result != NULL);
+  static void* _allocationResult;
 
-  return result;
-}
+  inline static void Test_initAllocationRecords() {
+    _allocationCount = 0;
+    _freeCount = 0;
+  }
 
-inline void Memory_free(void* ptr) {
-  assert(ptr != NULL);
-  free(ptr);
-}
+  inline static void Test_setAllocationResult(void* ptr) {
+    _allocationResult = ptr;
+  }
+
+  inline static void* Memory_allocate(size_t size) {
+    assert(_allocationCount < TEST_MAX_ALLOCATIONS);
+    _allocationList[_allocationCount++] = size;
+    return _allocationResult;
+  }
+
+  inline static void Memory_free(void* ptr) {
+    assert(_freeCount < _allocationCount);
+    assert(_freeCount < TEST_MAX_ALLOCATIONS);
+    _freeList[_freeCount++] = ptr;
+  }
+#else
+  inline static void* Memory_allocate(size_t size) {
+    void* result = malloc(size);
+
+    // TODO Handle this
+    assert(result != NULL);
+
+    return result;
+  }
+
+  inline static void Memory_free(void* ptr) {
+    assert(ptr != NULL);
+    free(ptr);
+  }
+#endif
 
 #endif
